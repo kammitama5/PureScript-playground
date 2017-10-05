0b8a3bd11d656e53fd6567af6b70f4c155afb1f4
kammitama5 <krystal.maughan@gmail.com>
2017-10-05 16:38:31 -0700

    simple functions (perim, divme, addme..ints only)

---
+++ PerimDivme.purs
@@ -0,0 +1,17 @@
+module Main where
+
+import Prelude
+
+-- initial addme function 
+
+addme :: Int -> Int -> Int 
+addme a b = a + b 
+
+
+-- divme function integers only
+divme :: Int -> Int -> Int 
+divme a b = a / b
+
+-- perimeter function 
+perim :: Int -> Int -> Int 
+perim l w = (l * 2) + (w * 2)
